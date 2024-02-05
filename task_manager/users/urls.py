f558bd398683b23b7e3802bdbc6b21077b0cc6a2
ai_gelios <ai_gelios@mail.ru>
2024-02-06 00:08:34 +0500

    add views and templates for user updation and deletion without authorization

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import (
     UsersIndexView,
     UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
 )
 
 
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users_index'),
     path('create/', UserCreateView.as_view(), name='create_user'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='update_user'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete_user'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -3,6 +3,8 @@ from .models import User
 from django.views.generic import (
     ListView,
     CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from django.contrib.messages.views import SuccessMessageMixin
@@ -10,6 +12,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from .forms import UserForm
 
 from django.utils.translation import gettext_lazy
+from django.urls import reverse_lazy
 
 
 class UsersIndexView(ListView):
@@ -23,3 +26,18 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserForm
     success_url = '/'
     success_message = gettext_lazy('Registration successful')
+
+
+class UserUpdateView(SuccessMessageMixin, UpdateView):
+    template_name = 'users/update.html'
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users_index')
+    success_message = gettext_lazy('User updation successful')
+
+
+class UserDeleteView(SuccessMessageMixin, DeleteView):
+    template_name = 'users/delete.html'
+    model = User
+    success_url = reverse_lazy('users_index')
+    success_message = gettext_lazy('User deletion successful')
